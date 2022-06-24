ab49eba3d4d480b864220e4528e03b74c426ac16
lsolani <70117286+MysteriousLychee@users.noreply.github.com>
2022-06-24 11:15:11 -0700

    Quality Changes to Poison Ivy Block

--- src/main/java/io/github/mysteriouslychee/dangerousagriculture/common/block/PoisonIvyBlock.java
+++ src/main/java/io/github/mysteriouslychee/dangerousagriculture/common/block/PoisonIvyBlock.java
@@ -1,16 +1,15 @@
 package io.github.mysteriouslychee.dangerousagriculture.common.block;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.VineBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.Vec3;
 
 public class PoisonIvyBlock extends VineBlock {
-
-	public static final DamageSource POISON_IVY_DAMAGE_SRC = new DamageSource("poisonIvyBlock");
 
 	public PoisonIvyBlock(Properties properties) {
 		super(properties);
@@ -18,16 +17,17 @@ public class PoisonIvyBlock extends VineBlock {
 
 	@Override
 	public void entityInside(BlockState state, Level lvl, BlockPos pos, Entity entity) {
-		entity.makeStuckInBlock(state, new Vec3((double) 0.8F, 0.75D, (double) 0.8F));
 
 		if (!lvl.isClientSide && (entity.xOld != entity.getX() || entity.zOld != entity.getZ())) {
 			double d0 = Math.abs(entity.getX() - entity.xOld);
 			double d1 = Math.abs(entity.getZ() - entity.zOld);
 			
 			if (d0 >= (double) 0.003F || d1 >= (double) 0.003F) {
-				entity.hurt(POISON_IVY_DAMAGE_SRC, 0.5F);
+				if (entity.isAlive()) {
+					LivingEntity livingEntity = (LivingEntity) entity;
+					livingEntity.addEffect(new MobEffectInstance(MobEffects.POISON, 100, 0, false, false, false));
+				}
 			}
 		}
 	}
-
 }
